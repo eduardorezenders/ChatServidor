395b826b4c2ab8d556a10cd4bf917dbbb5a026ca
eduardorezenders <eduardo.rezenders@gmail.com>
2022-03-25 09:50:03 -0300

    Adicionar arquivos de projeto.

---
+++ ChatServidor/frmServidor.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CRM.CHATSERVER
+{
+    partial class frmCHATSERVER
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCHATSERVER));
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 83);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(801, 363);
+            this.txtLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Server : ";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.Location = new System.Drawing.Point(94, 30);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(179, 20);
+            this.txtIP.TabIndex = 2;
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.Location = new System.Drawing.Point(559, 47);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(124, 23);
+            this.btnAtender.TabIndex = 3;
+            this.btnAtender.Text = "Iniciar Monitoramento";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnParar.Location = new System.Drawing.Point(689, 47);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(124, 23);
+            this.btnParar.TabIndex = 4;
+            this.btnParar.Text = "Parar Monitoramento";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port Server : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(94, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(62, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // frmCHATSERVER
+            // 
+            this.AcceptButton = this.btnAtender;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.CancelButton = this.btnParar;
+            this.ClientSize = new System.Drawing.Size(825, 458);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCHATSERVER";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRM.CHATSERVER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ChatServidor/frmServidor.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows.Forms;
+using System.Net;
+using System;
+
+namespace CRM.CHATSERVER
+{
+    public partial class frmCHATSERVER : Form
+    {
+        private delegate void AtualizaStatusCallback(string strMensagem);
+        private readonly string IP = Properties.Settings.Default.IP;
+        private readonly string PORTA = Properties.Settings.Default.PORTA;
+
+        private CHATSERVER mainServidor = new CHATSERVER(null, 0);
+
+        public frmCHATSERVER()
+        {
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            InitializeComponent();
+
+            txtIP.Text = IP;
+            textBox1.Text = PORTA;
+            btnAtender_Click(null, null);
+        }
+
+        private void btnAtender_Click(object sender, System.EventArgs e)
+        {
+            if (txtIP.Text == string.Empty)
+            {
+                _ = MessageBox.Show("Informe o endereço IP.");
+                _ = txtIP.Focus();
+                return;
+            }
+
+            try
+            {
+                // Analisa o endereço IP do servidor informado no textbox
+                IPAddress enderecoIP = IPAddress.Parse(txtIP.Text);
+
+                // Cria uma nova instância do objeto CRM.CHATSERVER
+                mainServidor = new CHATSERVER(enderecoIP, int.Parse(textBox1.Text));
+
+                // Vincula o tratamento de evento StatusChanged a mainServer_StatusChanged
+                CHATSERVER.StatusChanged += new StatusChangedEventHandler(mainServidor_StatusChanged);
+
+                // Inicia o atendimento das conexões
+                mainServidor.IniciaAtendimento(true);
+
+                // Mostra que nos iniciamos o atendimento para conexões
+                txtLog.AppendText("Monitoramento iniciado...\r\n");
+                btnAtender.Enabled = false;
+                btnParar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show("Erro de conexão : " + ex.Message);
+            }
+        }
+
+        public void mainServidor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Chama o método que atualiza o formulário
+            _ = Invoke(new AtualizaStatusCallback(AtualizaStatus), new object[] { e.EventMessage });
+        }
+
+        private void AtualizaStatus(string strMensagem)
+        {
+            // Atualiza o logo com mensagens
+            txtLog.AppendText(strMensagem + "\r\n");
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            txtLog.AppendText("Monitoramento parado...\r\n");
+            btnAtender.Enabled = true;
+            btnParar.Enabled = false;
+        }
+
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
